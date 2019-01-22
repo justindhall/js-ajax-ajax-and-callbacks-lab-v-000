3772c57a4ef4f7607108994a2d5a8721208fb124
Justin Hall <hall.justin.d@gmail.com>
2019-01-22 05:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = "https://api.github.com";
 
 function searchRepositories(){
-  const searchTarget = $("#seachBox").value;
+  const searchTarget = $("#seachTerms").value;
   const searchUrl = `${url}/search/repositories?q=${searchTarget}`;
 
   $.get(searchUrl, data => displayRepositories(data)).fail(error => displayError());
