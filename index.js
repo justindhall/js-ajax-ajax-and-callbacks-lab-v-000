19da0bbf72c5b1b03b553f0f26c5d7cc65b1a055
Justin Hall <hall.justin.d@gmail.com>
2019-01-22 05:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const url = "https://api.github.com";
+const base = "https://api.github.com"
 
-function searchRepositories(){
-  const searchTerms = $("#searchTerms").val();
-  const searchUrl = `${url}/search/repositories?q=${searchTerms}`;
-
-  $.get(searchUrl, data => displayRepositories(data)).fail(error => displayError());
-}
-
-function displayRepositories(results){
-  const repos = results.items.map(result => {
-    return `<div><a href="${result.html_url}">${result.name}</a>
-    <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-    <p>${result.description}</p>
-    </div>`
-  });
-
-  $("#results").html(repos);
-}
-
-function displayError(){
+function displayError() {
   $("#errors").html("I'm sorry, there's been an error. Please try again.");
 }
 
-function showCommits(el){
-  const owner = el.dataset.owner;
-  const repo = el.dataset.repository;
-  const searchUrl = `${url}/repos/${owner}/${repo}/commits`;
-  $.get(searchUrl, data => displayCommits(data)).fail(error => displayError());
+function searchRepositories() {
+  const searchTerms = $("#searchTerms").val();
+  const searchUrl = `${base}/search/repositories?q=${searchTerms}`;
+
+  $.get(searchUrl, data => displaySearchResults(data)).fail(error => displayError());
 }
 
-function displayCommits(data){
-  const commits = data.map(commit => `<li>${commit.sha}<p>${commit.commit.message}</p></li>)`.join("");
-  const commitsString = `<ul>${commits}</ul>`;
-  $("details").html(commitsString);
+function displaySearchResults(results) {
+  const resultString = results.items.map(result => {
+    return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+      `
+  });
+
+  console.log(resultString);
+
+  $("#results").html(resultString);
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner;
+  const repo = el.dataset.repository;
+  const url = `${base}/repos/${owner}/${repo}/commits`;
+  $.get(url, data => displayCommits(data)).fail(error => displayError());
+}
+
+function displayCommits(data) {
+  const commits = data.map(commit => `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`).join("");
+  const commitsString = `<ul>${commits}</ul>`
+  $("#details").html(commitsString);
 }
 
 $(document).ready(function (){
